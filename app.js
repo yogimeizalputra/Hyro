5ffcb8995662be36f4d95af93845375e1dab65cc
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-09-29 17:33:37 +0700

    Connecting to Database MangoDB

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
 
+// connect to mangoDB
+const dbURI = "mongodb+srv://yogimp:yogi123@hyroblogs.ln4eb.mongodb.net/hyro-blogs?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => app.listen(3000, () => {
+        console.log('Server is running on http://localhost:3000/'); 
+    }))
+    .catch(err => console.log(err));
+
 // register view engine
 app.set('view engine', 'ejs');
-
-// listen for requests
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000/'); 
-});
 
 // middleware $ static files
 app.use(express.static('public'));
