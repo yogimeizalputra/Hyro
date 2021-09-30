3e37e95ea95174db6bcc5068025e046ce41dd774
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-09-30 13:22:53 +0700

    Deleting Request

--- app.js
+++ app.js
@@ -64,6 +64,17 @@ app.get('/blogs/:id', (req, res) => {
         });
 });
 
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then( result => {
+            res.json({ redirect: '/blogs' })
+        })
+        .catch( err => {
+            console.log(err.message); 
+        });
+});
+
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new blog' });
 });
