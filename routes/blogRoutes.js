cf0a93f8ae9e596528cc1cbe792eefff91e3e520
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-09-30 16:35:34 +0700

    Controller Blogs

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,54 +1,12 @@
 const express = require('express');
-const Blog = require('../models/blog');
+const blogController = require('../controllers/blogController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then( result => {
-            res.render('index', { title: 'All Blogs', blogs: result})
-        })
-        .catch( err => {
-            console.log(err.message); 
-        });
-});
-
-router.post('/', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then( result => {
-            res.redirect('/blogs')
-        })
-        .catch( err => {
-            console.log(err.message); 
-        });
-});
-
-router.get('/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-});
-
-router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then( result => {
-            res.render('details', { title: 'Blog Details' , blog: result })
-        })
-        .catch( err => {
-            console.log(err.message); 
-        });
-});
-
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then( result => {
-            res.json({ redirect: '/blogs' })
-        })
-        .catch( err => {
-            console.log(err.message); 
-        });
-});
+router.get('/create', blogController.blog_create_get);
+router.get('/', blogController.blog_index);
+router.post('/', blogController.blog_create_post);
+router.get('/:id', blogController.blog_details);
+router.delete('/:id', blogController.blog_delete);
 
 module.exports = router;
